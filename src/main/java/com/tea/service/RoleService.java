d6f09d3cd7ef3f7e36e4943d0f6cf9c6a14bf580
unknown <1009154918@qq.com>
2019-06-09 13:12:22 +0800

    update role -findById

--- src/main/java/com/tea/service/RoleService.java
+++ src/main/java/com/tea/service/RoleService.java
@@ -1,11 +1,13 @@
 package com.tea.service;
 
 import com.tea.entity.Role;
+import com.tea.entity.Spec;
 import com.tea.repository.RoleRepository;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoleService {
@@ -23,5 +25,13 @@ public class RoleService {
 
     public List<Role> findAll(){
         return roleRepository.findAll();
+    }
+    public  Role  findById(Integer id){
+        Optional<Role> role=roleRepository.findById(id);
+        if (role.isPresent()){
+            return role.get();
+        }else {
+            return null;
+        }
     }
 }
